8da40ba3ff6d762951a20881cfa15677e6a4e18a
xiaopeng.guo <guoxiaopeng@kingsoft.com>
2018-12-04 11:13:30 +0800

    fadsf

---
+++ src/main/java/com/example/demo/SecondCladd.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class SecondCladd {
+
+	public static void todString() {
+		System.out.println("haha");
+	}
+}
--- src/main/java/com/example/demo/TestRequesController.java
+++ src/main/java/com/example/demo/TestRequesController.java
@@ -9,12 +9,15 @@ public class TestRequesController {
 	//localhost:6093/testhotload
 	@RequestMapping("testhotload")
 	private String RequestTest(){
+		SecondCladd.todString();
 		StringBuilder sb = new StringBuilder();
 		sb.append("before host load");
 
 		sb.append("\n");
 
 //		sb.append("after host load  NO ERROR???");
+//		sb.append(FDSFDFDFDSF)
+		SecondCladd.todString();
 
 		sb.append("\n");
 		sb.append("逻辑后重新实验");
